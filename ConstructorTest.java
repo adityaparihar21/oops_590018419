54076dbf3e63b2b4298b400d966659f0d152ec24
ADITYA PARIHAR <parihar21aditya@gmail.com>
2026-02-11 08:45:38 +0530

    Add files via upload

---
+++ ConstructorTest.java
@@ -0,0 +1,11 @@
+class Helper {
+    Helper() {
+        System.out.println("Constructor Invoked");
+    }
+}
+
+public class ConstructorTest {
+    public static void main(String[] args) {
+        new Helper();
+    }
+}
