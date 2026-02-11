a66a269bc5a5c8e450f50e15cf67c88dada8a791
ADITYA PARIHAR <parihar21aditya@gmail.com>
2026-02-11 08:41:47 +0530

    Add files via upload

---
+++ CmdStringArray.java
@@ -0,0 +1,8 @@
+public class CmdStringArray {
+    public static void main(String[] args) {
+        String[] strArr = args;
+        for(String s : strArr) {
+            System.out.println(s);
+        }
+    }
+}
